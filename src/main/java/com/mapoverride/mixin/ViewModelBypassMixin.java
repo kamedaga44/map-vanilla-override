5d7a8a96e9a4dcb8c3d5ab2afceea22f05620c68
kame_44 <124362749+kamedaga44@users.noreply.github.com>
2026-09-16 21:10:58 +0900

    Update ViewModelBypassMixin.java

--- src/main/java/com/mapoverride/mixin/ViewModelBypassMixin.java
+++ src/main/java/com/mapoverride/mixin/ViewModelBypassMixin.java
@@ -10,13 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(
-    targets = {
-        "com.viewmodel.mixin.HeldItemTransformMixin",
-        "com.viewmodel.mixin.HeldItemModelTransformMixin"
-    },
-    remap = false
-)
+@Mixin(value = Object.class, remap = false)
 public class ViewModelBypassMixin {
 
     @Inject(method = "onApplyTransforms", at = @At("HEAD"), cancellable = true, require = 0)
